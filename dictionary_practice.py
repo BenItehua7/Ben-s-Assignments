f269454e6655564b6069ce47624ac66ed3d2b00c
BenItehua7 <103339971+BenItehua7@users.noreply.github.com>
2022-04-24 18:42:28 -0700

    updated  dict practice
    finished practice with dictionaries

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -1,1 +1,46 @@
-dict_name = {"chicken":1.59}, {"beef":1.99},{"cheese":1.00},{"milk":2.50}
+from multiprocessing.pool import ApplyResult
+from this import d
+
+
+soccer_players = {"lionel messi":30,"christiano ronaldo":7,"neymar":10,"marcelo viera":12}
+
+messi_jersey = soccer_players["lionel messi"]
+ronaldo_jersey = soccer_players["christiano ronaldo"]
+neymar_jersey = soccer_players["neymar"]
+marcelo_jersey = soccer_players["marcelo viera"]
+
+#print(messi_jersey, ronaldo_jersey, neymar_jersey, marcelo_jersey)
+
+shoes = {"jordan" : 10, "yeezy" : 10, "foamposite" : 10, "air max" : 10, "sb dunk" : 10}
+
+print(shoes)
+
+shoes  ["sb dunk"] -= 2
+
+shoes ["yeezy"] += 1
+
+shoes ["jordan"] += 7
+shoes ["yeezy"] += 7
+shoes ["foamposite"] += 7
+shoes ["air max"] += 7
+shoes ["sb dunk"] += 7
+
+print(shoes)
+
+shoes ["jordan"] -=3
+shoes ["yeezy"] -=3
+shoes ["foamposite"] -=3
+shoes ["air max"] -=3
+shoes ["sb dunk"] -=3
+
+print(shoes) 
+soccer_players ['javier hernandez'] ='14'
+print (soccer_players) 
+
+soccer_players ['lozano'] ='22'
+soccer_players ['sergio ramos'] ='4'
+
+print (soccer_players)
+
+del soccer_players["neymar"]
+print(soccer_players)
