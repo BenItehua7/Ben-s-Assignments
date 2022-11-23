a1fab766293932f766271c6612fe871b7857fddb
BenItehua7 <103339971+BenItehua7@users.noreply.github.com>
2022-11-22 19:45:31 -0800

    Create mfpUptated.py

---
+++ LaneyCIS/mfpUptated.py
@@ -0,0 +1,10 @@
+name = input ("Please Enter Your Name: ") 
+age = int(input("How old are you?: "))
+
+if 0 <= age <= 100:
+    print (name + " will be " , age + 5 , "in Five years")
+elif age <=0:
+    print("People cannot have a negative age")
+elif age >= 100:
+    print("100 is the maximum age for this program")
+    
