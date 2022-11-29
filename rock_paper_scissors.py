dc56810aed4f06634e06130b4ca0da73a0c4e4f6
BenItehua7 <103339971+BenItehua7@users.noreply.github.com>
2022-11-29 11:11:30 -0800

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,19 +10,19 @@ while True:
         print(f"Both players selected {user_action}. It's a tie!")
     elif user_action == "rock":
         if computer_action == "scissors":
-            print("Rock smashes scissors! You win!")
+            print("Rock smashes scissors. You win!")
         else:
-            print("Paper covers rock! You lose.")
+            print("Paper covers rock. You lose.")
     elif user_action == "paper":
         if computer_action == "rock":
-            print("Paper covers rock! You win!")
+            print("Paper covers rock. You win!")
         else:
-            print("Scissors cuts paper! You lose.")
+            print("Scissors cuts paper. You lose.")
     elif user_action == "scissors":
         if computer_action == "paper":
-            print("Scissors cuts paper! You win!")
+            print("Scissors cuts paper. You win!")
         else:
-            print("Rock smashes scissors! You lose.")
+            print("Rock smashes scissors. You lose.")
 
     play_again = input("Play again? (y/n): ")
     if play_again.lower() != "y":
