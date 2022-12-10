e0ffd7d090d95d0a2f588b36fa3ed5005e93019d
BenItehua7 <103339971+BenItehua7@users.noreply.github.com>
2022-12-10 00:53:30 -0800

    Update bankacc.py
    11

--- LaneyCIS/bankacc.py
+++ LaneyCIS/bankacc.py
@@ -1,28 +1,44 @@
-User_Name_Input = (input("Enter your name "))
-print ("Hello", User_Name_Input,("Welcome to your Bank Account"))
-
-balance = float(input("Enter starting balance: "))
-print ("Starting balance: ", balance)
-menu_control = int(input("Do you want to Deposit (1) or withdrawal (2): "))
-
-if menu_control == 1:
-    deposit = float(input("Enter amount you want to deposit: "))
-    if deposit > 0:
-        balance = balance + deposit
-        print("New balance = ", balance)
-    else:
-     print("Invalid choice")
-
-    print("Choice 1 (Deposit) entered")
-elif menu_control == 2:
-    print ("Choice 2 (Withdrawal) entered")
-    withdrawal = float(input("Enter amount you want to withdrawal: "))
-    if balance >= withdrawal:
-     balance = balance- withdrawal
-     print("You Withdrew: ", balance)
-    else:  
-      print("Invalid withdrawal")
-    print(User_Name_Input + "New Balance = ", balance)
-else:
-    print("Invalid choice")
+def deposit(deposit_amount):
+    global balance
+    balance = balance + deposit_amount
     
+    if deposit_amount > 0:
+        balance + deposit_amount
+    else:
+        print("invalid input")
+
+def withdrawal(withdrawal_amount):
+    global balance 
+    if balance>withdrawal_amount:
+        balance =balance - withdrawal_amount
+    else:
+        print("invalid input ")
+
+def main():
+    global balance
+    User_Name_Input= input ("Enter Name: ")
+    print ("Hello", User_Name_Input, " Welcome to your Bank account")
+    balance = float(input("Enter starting balance: "))
+    print ("Starting balance: ", balance)
+    menu_control = int(input("Do you want to Deposit (1) or withdrawal (2): "))
+    
+    if menu_control == 1:
+        deposit = float(input("Enter amount you want to deposit: "))
+        if deposit > 0:
+            balance = balance + deposit
+            print("New balance = ", balance)
+        else:
+         print("Invalid choice")
+        print("Choice 1 (Deposit) entered")
+    elif menu_control == 2:
+        print ("Choice 2 (Withdrawal) entered")
+        withdrawal = float(input("Enter amount you want to withdrawal: "))
+        if balance >= withdrawal:
+         balance = balance- withdrawal
+         print("You Withdrew: ", balance)
+        else:  
+          print("Invalid withdrawal")
+        print(User_Name_Input + "'s New Balance = ", balance)
+    else:
+        print("Invalid choice")
+main()
