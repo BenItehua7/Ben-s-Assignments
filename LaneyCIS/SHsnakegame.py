2a0cc7db0d8aa6e29e1aaf54c90ef43527561ec3
BenItehua7 <103339971+BenItehua7@users.noreply.github.com>
2022-12-11 22:33:16 -0800

    Update SHsnakegame.py
    new

--- LaneyCIS/SHsnakegame.py
+++ LaneyCIS/SHsnakegame.py
@@ -1,100 +1,103 @@
 from sense_hat import SenseHat
-from time import sleep
+import time
 import random
-
-sense = SenseHat()
-sense.low_light = True
+    
+senseHat = SenseHat()
+senseHat.low_light = True
 
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
 START_DELAY = 3
-MATRIX_MIN_VALUE = 0
-MATRIX_MAX_VALUE = 7
-MATRIX_SIZE = 8
+MIN_VALUE = 0
+MAX_VALUE = 7
+SIZE = 8
 
 while True:
-    gameOverFlag = False
-    growSnakeFlag = False
-    generateRandomFoodFlag = False
+    # variables:
+    gameOver = False
+    growSnake = False
+    generateRandomFood = False
     snakeMovementDelay = 0.5
-    score = 0
+    snakeMovementDelayDecrease = -0.02
 
-    sense.clear()
-    sense.show_message("SNAKE GAME")
+    # start delay:
+    time.sleep(START_DELAY)
 
-    # Set default snake starting position (values are chosen by preference)
+    # set default snake starting position (values are chosen by preference):
     snakePosX = [3]
     snakePosY = [6]
 
-    # Generate random food position
+    # generate random food position:
     while True:
         foodPosX = random.randint(0, 7)
         foodPosY = random.randint(0, 7)
         if foodPosX != snakePosX[0] or foodPosY != snakePosY[0]:
             break
 
-    # Set default snake starting direction (values are chosen by preference)
+    # set default snake starting direction (values are chosen by preference):
     movementX = 0
-    movementY = 1
+    movementY = -1
 
-    while not gameOverFlag:
-        # Check if snake eats food
+    # -----------------------------------
+    #             game loop
+    # -----------------------------------
+    while not gameOver:
+        # check if snake eats food:
         if foodPosX == snakePosX[0] and foodPosY == snakePosY[0]:
-            growSnakeFlag = True
-            generateRandomFoodFlag = True
+            growSnake = True
+            generateRandomFood = True
             snakeMovementDelay += snakeMovementDelayDecrease
-            score = score + 1
 
-        # Check if snake bites itself
+        # check if snake bites itself:
         for i in range(1, len(snakePosX)):
             if snakePosX[i] == snakePosX[0] and snakePosY[i] == snakePosY[0]:
-                gameOverFlag = True
+                gameOver = True
 
-        # Check if game-over
-        if gameOverFlag:
+        # check if game-over:
+        if gameOver:
             break
 
-        # Check joystick events
-        for event in sense.stick.get_events():
+        # check joystick events:
+        events = senseHat.stick.get_events()
+        for event in events:
             if event.direction == "left" and movementX != 1:
-                movementX = 1
+                movementX = -1
                 movementY = 0
-            elif event.direction == "right" and movementX != 1:
+            elif event.direction == "right" and movementX != -1:
                 movementX = 1
                 movementY = 0
             elif event.direction == "up" and movementY != 1:
-                movementY = 1
+                movementY = -1
                 movementX = 0
-            elif event.direction == "down" and movementY != 1:
+            elif event.direction == "down" and movementY != -1:
                 movementY = 1
                 movementX = 0
 
-        # Grow snake
-        if growSnakeFlag:
-            growSnakeFlag = False
+        # grow snake:
+        if growSnake:
+            growSnake = False
             snakePosX.append(0)
             snakePosY.append(0)
 
-        # Move snake
-        for i in range((len(snakePosX)  1), 0, 1):
-            snakePosX[i] = snakePosX [i – 1]
-            snakePosY[i] = snakePosY [i – 1]
+        # move snake:
+        for i in range((len(snakePosX) - 1), 0, -1):
+            snakePosX[i] = snakePosX[i - 1]
+            snakePosY[i] = snakePosY[i - 1]
 
         snakePosX[0] += movementX
         snakePosY[0] += movementY
 
-        # Check game borders
-        if snakePosX[0] > MATRIX_MAX_VALUE:
-            snakePosX[0] -= MATRIX_SIZE
-        elif snakePosX[0] < MATRIX_MIN_VALUE:
-            snakePosX[0] += MATRIX_SIZE
-        if snakePosY[0] > MATRIX_MAX_VALUE:
-            snakePosY[0] -= MATRIX_SIZE
-        elif snakePosY[0] < MATRIX_MIN_VALUE:
-            snakePosY[0] += MATRIX_SIZE
+        # check game borders:
+        if snakePosX[0] > MAX_VALUE:
+            snakePosX[0] -= SIZE
+        elif snakePosX[0] < MIN_VALUE:
+            snakePosX[0] += SIZE
+        if snakePosY[0] > MAX_VALUE:
+            snakePosY[0] -= SIZE
+        elif snakePosY[0] < MIN_VALUE:
+            snakePosY[0] += SIZE
 
-        # Spawn random food
+        # spawn random food:
         if generateRandomFoodFlag:
             generateRandomFoodFlag = False
             retryFlag = True
@@ -107,37 +110,11 @@ while True:
                         retryFlag = True
                         break
 
-        # Update matrix
-        sense.clear()
-        sense.set_pixel(foodPosX, foodPosY, RED)
+        # update matrix:
+        senseHat.clear()
+        senseHat.set_pixel(foodPosX, foodPosY, RED)
         for x, y in zip(snakePosX, snakePosY):
-            sense.set_pixel(x, y, GREEN)
+            senseHat.set_pixel(x, y, GREEN)
 
-        # Snake speed (game loop delay)
-        sleep(snakeMovementDelay)
-
-    # Blink the dead snake
-    for loop in range (5):
-        sense.clear()
-        sense.set_pixel(foodPosX, foodPosY, RED)
-        for x, y in zip(snakePosX, snakePosY):
-            sense.set_pixel(x, y, RED)
-        sleep(0.5)
-        
-        sense.clear()
-        sense.set_pixel(foodPosX, foodPosY, RED)
-        for x, y in zip(snakePosX, snakePosY):
-            sense.set_pixel(x, y, GREEN)
-        sleep(0.5)
-
-    sense.clear()
-
-    # Display score
-    while score:
-        sense.show_message("Score: {}".format(score), text_colour=YELLOW)
-        # Press joystick middle button to play again
-        for event in sense.stick.get_events():
-            if event.direction == "middle":
-                score = 0
-                
-view   raw
+        # snake speed (game loop delay):
+        time.sleep(snakeMovementDelay)
